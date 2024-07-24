d2069811cd7d6ce538692cd5848762438d7782d9
Fine <92whdtjs@gmail.com>
2024-07-24 11:55:32 +0900

    MVP 구조 기본 세팅

---
+++ MVP-MyToDos/MVP-MyToDos/Model/CoreData/Task+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  Task+CoreDataProperties.swift
+//  MVP-MyToDos
+//
+//  Created by 이종선 on 7/24/24.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var id: String?
+    @NSManaged public var title: String?
+    @NSManaged public var icon: String?
+    @NSManaged public var done: Date?
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var list: TasksList?
+
+}
+
+extension Task : Identifiable {
+
+}
---
+++ MVP-MyToDos/MVP-MyToDos/Model/CoreData/TasksList+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TasksList+CoreDataClass.swift
+//  MVP-MyToDos
+//
+//  Created by 이종선 on 7/24/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TasksList)
+public class TasksList: NSManagedObject {
+
+}
