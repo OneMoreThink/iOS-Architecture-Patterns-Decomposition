faf65f87f7bdcf5c074a28b04922d223c63195b5
fine <92whdtjs@gmail.com>
2024-08-18 15:55:10 +0900

    FieldLabel 추가

--- MVC-MyToDos/MVC-MyToDos/View/Labels/FieldLabel.swift
+++ MVC-MyToDos/MVC-MyToDos/View/Labels/FieldLabel.swift
@@ -5,4 +5,21 @@
 //  Created by 이종선 on 8/17/24.
 //
 
-import Foundation
+import UIKit
+
+class FieldLabel: UILabel {
+
+    required init(title: String) {
+        super.init(frame: .zero)
+
+        translatesAutoresizingMaskIntoConstraints = false
+        textAlignment = .left
+        font = .systemFont(ofSize: 21, weight: .light)
+        textColor = .grayTextColor
+        text = title
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
