4badbc59cdf7d942f24192ff2f6d98431df9d034
fine <92whdtjs@gmail.com>
2025-01-03 20:04:04 +0900

    MVVM: CoreData 엔티티 설정

---
+++ MVVM_MyToDos/MVVM_MyToDos/Model/CoreData/Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  MVVM_MyToDos
+//
+//  Created by 이종선 on 1/3/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
---
+++ MVVM_MyToDos/MVVM_MyToDos/Model/CoreData/TasksList+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TasksList+CoreDataClass.swift
+//  MVVM_MyToDos
+//
+//  Created by 이종선 on 1/3/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TasksList)
+public class TasksList: NSManagedObject {
+
+}
